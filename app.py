efc0b41296860159cf4f46344ba77414932207b2
Kasum <kasum2605@mail.ru>
2024-10-29 01:04:47 +0400

    few changes

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from typing import AsyncGenerator
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 
 from src.api import router as api_router
+from src.common import config
 
 
 @asynccontextmanager
@@ -16,6 +18,14 @@ async def lifespan(_app: FastAPI) -> AsyncGenerator:
 
 
 app = FastAPI(title='Market API', lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[config.ORIGIN_URL],
+    allow_credentials=True,
+    allow_methods=['GET', 'POST', 'PUT'],
+    allow_headers=['*'],
+)
 
 app.include_router(api_router, prefix='/api')
 
